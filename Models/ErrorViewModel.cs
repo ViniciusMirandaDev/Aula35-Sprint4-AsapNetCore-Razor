f16a27f8809d9916ebd61d013527d9b35404d139
ViniciusMirandaDev <ViniciusMirandaDev>
2020-07-09 14:55:47 -0300

    Lição Finalizada

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aula35_Sprint4_AsapNetCore_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
